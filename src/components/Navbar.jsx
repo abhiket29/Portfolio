409575b4d5605212812483677a4c6f439f62bdf5
abhiket29 <abhiket29@gmail.com>
2025-01-14 22:00:30 +0530

    animation done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import Arlogo from "../assets/ARlogo.jpg";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <nav className="mb-20 flex items-center justify-between py-6">
+        <div className="flex flex-shrink-0 items-center">
+          <img className="mx-2 w-20 h-30 rounded-3xl" src={Arlogo} alt="logo" />
+        </div>
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+          <a
+            href="http://www.linkedin.com/in/abhiket-kumar-28964b238"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://github.com/abhiket29"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://www.instagram.com/_.abhiiket.krr.49__/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Instagram"
+          >
+            <FaInstagram />
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+};
