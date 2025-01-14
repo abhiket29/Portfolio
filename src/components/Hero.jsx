2c7892f6358fe95cd518bd9dcd4343ab9229bee4
abhiket29 <abhiket29@gmail.com>
2025-01-15 05:00:17 +0530

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
               animate="visible"
               className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
             >
-              Abhiket Kr Roy
+              Abhiket Roy
             </motion.h1>
             <motion.span
               variants={container(0.5)}
